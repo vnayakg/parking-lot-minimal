7775c5f9f12797d3d98b397656eca0a2decb5203
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-02-17 00:26:57 +0530

    Add unPark method in ParkingLot

--- src/main/kotlin/ParkingLot.kt
+++ src/main/kotlin/ParkingLot.kt
@@ -1,10 +1,13 @@
-import model.Ticket
-import model.Vehicle
+import model.*
+import repository.ParkingSpotRepository
 import java.util.Date
 
-class ParkingLot(private val parkingSpotRepository: ParkingSpotRepository) {
+class ParkingLot(private val parkingSpotRepository: ParkingSpotRepository,
+    private val feeCalculator: FeeScheme
+) {
 
     private var ticketNumber = 0
+    private var receiptNumber = 0
     fun park(vehicle: Vehicle, currentDateTime: Date): Ticket {
         val vehicleType = vehicle.getVehicleType()
 
@@ -19,6 +22,33 @@ class ParkingLot(private val parkingSpotRepository: ParkingSpotRepository) {
             nextAvailableParkingSpot.getSpotNumber(),
             currentDateTime
         )
+    }
+
+    fun unPark(ticket: Ticket, exitDateTime: Date): Receipt {
+        val spot = parkingSpotRepository.getSpotByVehicleNumber(
+            ticket.getVehicleType(),
+            ticket.getVehicleLicenseNumber()
+        )
+
+        spot.vacateSlot()
+
+        val parkHours = calculateParkHours(ticket.getIssueDateTime(), exitDateTime)
+        val parkingFee = feeCalculator.calculateFee(parkHours, ticket.getVehicleType())
+
+        return Receipt(
+            ++receiptNumber,
+            ticket.getVehicleLicenseNumber(),
+            ticket.getIssueDateTime(),
+            exitDateTime,
+            parkingFee
+        )
+    }
+
+    private fun calculateParkHours(issueDateTime: Date, exitDateTime: Date): Float {
+        val timeDifferenceInMilliSeconds = exitDateTime.time - issueDateTime.time
+        val milliSecondsPerHour = 1000f * 60f * 60f
+
+        return timeDifferenceInMilliSeconds / milliSecondsPerHour
     }
 
 }
--- src/test/kotlin/ParkingLotTest.kt
+++ src/test/kotlin/ParkingLotTest.kt
@@ -1,13 +1,18 @@
+import model.FeeCalculator
 import model.Vehicle
 import model.VehicleType
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import repository.ParkingSpotRepository
 import java.util.*
 
 class ParkingLotTest {
+
     private val vehicleSpotCapacity = mapOf(VehicleType.CAR to 100)
     private val parkingSpotRepository = ParkingSpotRepository(vehicleSpotCapacity)
-    private val parkingLot = ParkingLot(parkingSpotRepository)
+    private val feeCalculator = FeeCalculator()
+    private val parkingLot = ParkingLot(parkingSpotRepository, feeCalculator)
+
     @Test
     fun `it should park vehicle`() {
         val vehicle = Vehicle(VehicleType.CAR, "MH 12 AB 1234")
@@ -16,5 +21,20 @@ class ParkingLotTest {
 
         assertEquals("MH 12 AB 1234", ticket.getVehicleLicenseNumber())
         assertEquals(issueDateTime, ticket.getIssueDateTime())
+    }
+
+    @Test
+    fun `it should un park vehicle`() {
+        val vehicle = Vehicle(VehicleType.CAR, "MH 12 AB 1234")
+        val entryDateTime = Date()
+        val milliSecondsInHour = 3600000
+        val exitDateTime = Date(entryDateTime.time + milliSecondsInHour)
+        val ticket = parkingLot.park(vehicle, entryDateTime)
+
+        val receipt = parkingLot.unPark(ticket, exitDateTime)
+
+        assertEquals("MH 12 AB 1234", ticket.getVehicleLicenseNumber())
+        assertEquals(entryDateTime, ticket.getIssueDateTime())
+        assertEquals(10.0F, receipt.getFee())
     }
 }

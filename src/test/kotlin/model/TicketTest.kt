42f3b05eb867f1f185cd0d0ed069cb4fd57298de
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-02-16 22:33:48 +0530

    Add Ticket class

---
+++ src/test/kotlin/model/TicketTest.kt
@@ -0,0 +1,27 @@
+package model
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class TicketTest {
+    @Test
+    fun `should get ticket information`() {
+        val vehicle = Vehicle(VehicleType.CAR, "MH 12 AB 1234")
+        val issueDateTime = Date()
+
+        val ticket = Ticket(1, vehicle, 1, issueDateTime)
+        val expectedTicketInformation = ticket.toString()
+
+        val actualTicketInformation = """
+            ""${'"'}
+            Ticket\n
+            TicketNumber: 1\n
+            Spot Number: 1\n
+            Entry DateTime: $issueDateTime
+            ""${'"'}
+        """.trimIndent()
+
+        assertEquals(expectedTicketInformation, actualTicketInformation)
+    }
+}

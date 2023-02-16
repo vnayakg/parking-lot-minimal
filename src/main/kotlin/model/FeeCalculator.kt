bdb4c56b6585c6eb21fb506cd24367aa74d22f31
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-02-17 00:11:46 +0530

    Add FeeCalculator to calculate total parking fee

---
+++ src/main/kotlin/model/FeeCalculator.kt
@@ -0,0 +1,16 @@
+package model
+
+import exception.InvalidTimeDurationException
+import exception.InvalidVehicleTypeException
+
+class FeeCalculator : FeeScheme {
+    private val perHourFee = 10
+    override fun calculateFee(parkDurationInHours: Float, vehicleType: VehicleType): Float {
+        if (parkDurationInHours <= 0) throw InvalidTimeDurationException("Park duration in hours should be positive")
+        when (vehicleType) {
+            VehicleType.CAR -> return perHourFee * parkDurationInHours
+            else -> throw InvalidVehicleTypeException("Vehicle type is invalid")
+        }
+    }
+
+}
---
+++ src/test/kotlin/model/FeeCalculatorTest.kt
@@ -0,0 +1,27 @@
+package model
+
+import exception.InvalidTimeDurationException
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class FeeCalculatorTest {
+    @Test
+    fun `it should calculate fee`() {
+        val feeCalculator = FeeCalculator()
+
+        val actualFee = feeCalculator.calculateFee(2.5F, VehicleType.CAR)
+
+        assertEquals(25F, actualFee)
+    }
+
+    @Test
+    fun `it should give throw exception for non-positive park duration`() {
+        val feeCalculator = FeeCalculator()
+
+        assertThrows(
+            InvalidTimeDurationException::class.java
+        ) {
+            feeCalculator.calculateFee(-2.5F, VehicleType.CAR)
+        }
+    }
+}

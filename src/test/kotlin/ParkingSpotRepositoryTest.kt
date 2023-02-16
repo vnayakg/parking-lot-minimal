8f96848a3cc5efa97d0c18ab936e77a195fe4ebe
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-02-16 23:26:16 +0530

    Change initialization of ParkingSpotRepository

--- src/test/kotlin/ParkingSpotRepositoryTest.kt
+++ src/test/kotlin/ParkingSpotRepositoryTest.kt
@@ -3,21 +3,11 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class ParkingSpotRepositoryTest {
-    @Test
-    fun `should initialize parking spots`() {
-        val vehicleSpotCapacity = mapOf(VehicleType.CAR to 100)
-        val parkingSpotRepository = ParkingSpotRepository()
-
-        val spots = parkingSpotRepository.initialize(vehicleSpotCapacity)
-
-        assertEquals(spots[VehicleType.CAR]?.size, 100)
-    }
 
     @Test
     fun `should be able to find next available spot`() {
         val vehicleSpotCapacity = mapOf(VehicleType.CAR to 100)
-        val parkingSpotRepository = ParkingSpotRepository()
-        parkingSpotRepository.initialize(vehicleSpotCapacity)
+        val parkingSpotRepository = ParkingSpotRepository(vehicleSpotCapacity)
 
         val nextAvailableSpot = parkingSpotRepository.getNextAvailableSpot(VehicleType.CAR)
 

1b6f2b3d43a591478eeb1c2af5ad158dd29570b1
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-02-16 22:56:02 +0530

    Add Spot class for parking spot

---
+++ src/main/kotlin/model/Spot.kt
@@ -0,0 +1,27 @@
+package model
+
+class Spot(private val spotNumber: Int, private var isEmpty: Boolean = true) {
+    private var vehicle: Vehicle? = null
+    fun getParkedVehicle(): Vehicle? {
+        return vehicle
+    }
+
+    fun allocateSlot(vehicle: Vehicle) {
+        this.vehicle = vehicle
+        isEmpty = false
+    }
+
+    fun vacateSlot() {
+        this.vehicle = null
+        isEmpty = true
+    }
+
+    fun isEmpty(): Boolean {
+        return isEmpty
+    }
+
+    fun getSpotNumber(): Int {
+        return spotNumber
+    }
+
+}

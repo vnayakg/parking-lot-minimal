9aca3ca931108fa51bf4e0d47ce2ff117f1035d2
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-02-18 23:02:43 +0530

    Refactor ParkingSpot: Remove unused methods

--- src/main/kotlin/model/ParkingSpot.kt
+++ src/main/kotlin/model/ParkingSpot.kt
@@ -3,27 +3,7 @@ package model
 class ParkingSpot(
     private val spotNumber: Int,
     private val vehicleType: VehicleType,
-    private var isEmpty: Boolean = true
 ) {
-    private var vehicle: Vehicle? = null
-    fun getParkedVehicle(): Vehicle? {
-        return vehicle
-    }
-
-    fun allocateSlot(vehicle: Vehicle) {
-        this.vehicle = vehicle
-        isEmpty = false
-    }
-
-    fun vacateSlot() {
-        this.vehicle = null
-        isEmpty = true
-    }
-
-    fun isEmpty(): Boolean {
-        return isEmpty
-    }
-
     fun getSpotNumber(): Int {
         return spotNumber
     }

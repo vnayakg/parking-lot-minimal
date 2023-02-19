7aa27d94f4428e514618ae8f1567e53306382b1d
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-02-19 14:49:56 +0530

    Refactor ParkingSpotList: Rename spots to availableSpots

--- src/main/kotlin/repository/ParkingSpotList.kt
+++ src/main/kotlin/repository/ParkingSpotList.kt
@@ -4,16 +4,16 @@ import exception.ParkingLotCapacityExceededException
 import model.ParkingSpot
 import model.VehicleType
 
-class ParkingSpotList(private val spots: ArrayList<ParkingSpot>) {
+class ParkingSpotList(private val availableSpots: ArrayList<ParkingSpot>) {
     fun getNextAvailableSpot(vehicleType: VehicleType): ParkingSpot {
         try {
-            return spots.first { spot -> spot.getVehicleType() == vehicleType }
+            return availableSpots.first { spot -> spot.getVehicleType() == vehicleType }
         } catch (exception: NoSuchElementException) {
             throw ParkingLotCapacityExceededException("No available slot for $vehicleType")
         }
     }
 
     fun vacateSpot(spot: ParkingSpot) {
-        spots.add(spot)
+        availableSpots.add(spot)
     }
 }
